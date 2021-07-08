a754092511400c34e8b8de3ae75f19c4d8dded7d
TylenRahl <thomas.hope88@gmail.com>
2021-07-08 11:01:38 +0100

    Tried to get quit button to work, does not work, something due to fonts, I think, therefore I am.

--- MainGUi.py
+++ MainGUi.py
@@ -163,6 +163,30 @@ def Quit_button():
     pygame.draw.rect(screen, color, pygame.Rect(190, 740, 120, 30))
     pygame.display.flip()
 
+    color_white = (255, 255, 255)
+    color_light = (180, 180, 180)
+    color_dark = (110, 110, 110)
+    width = screen.get_width()
+    height = screen.get_height()
+    smallfont = pygame.font.SysFont('Arial', 30)
+    text = smallfont.render('Quit', True, color_white)
+    while True:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                pygame.quit()
+            if e.type == pygame.MOUSEBUTTONDOWN:
+                if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:
+                    pygame.quit()
+
+        mouse = pygame.mouse.get_pos()
+        if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:
+            pygame.draw.rect(screen, color_light, [190, 740, 120, 30])
+        else:
+            pygame.draw.rect(screen, color_dark, [190, 740, 120, 30])
+        screen.blit(text, (width / 2 + 50, height / 2))
+        pygame.display.update()
+
+
 Quit_button()
 
 
