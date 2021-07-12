e45cda75712785a96aa723055bc16a2e84c4e19a
TylenRahl <thomas.hope88@gmail.com>
2021-07-12 13:38:36 +0100

    I have created new funtionc called "Dictionary of Places". On line 25. Not sure on the dictionary, so please take a look. Created main button function line 26 and player_struct line 52.1

--- MainGUi.py
+++ MainGUi.py
@@ -4,6 +4,7 @@ import numpy as np
 import random
 import time
 import sys
+from collections import namedtuple
 
 #Constants
 #DIMENSIONS
@@ -21,6 +22,50 @@ BG_COLOR = (172, 172, 172)
 screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
 pygame.display.set_caption( 'DrugLord' )
 screen.fill( BG_COLOR )
+
+def main_Buttons():
+    buttonStruct = namedtuple("buttonStruct", "x y width height name")
+
+
+    b1 = buttonStruct(x=190, y=250, width= 120, height=30, name="Buy")
+    b2 = buttonStruct(x=190, y=290, width=120, height=30, name="Sell")
+    b3 = buttonStruct(x=190, y=330, width=120, height=30, name="Dump")
+    b4 = buttonStruct(x=190, y=370, width=120, height=30, name="Places")
+    b5 = buttonStruct(x=190, y=410, width=120, height=30, name="Info")
+    b6 = buttonStruct(x=190, y=470, width=120, height=30, name="Stay")
+    b7 = buttonStruct(x=190, y=510, width=120, height=30, name="Fly")
+    b8 = buttonStruct(x=200, y=590, width=20, height=20, name="Sound")
+    b9 = buttonStruct(x=190, y=620, width=120, height=30, name="About")
+    b10 = buttonStruct(x=190, y=660, width=120, height=30, name="Docs")
+    b11 =buttonStruct(x=190, y=700, width=120, height=30, name="High_Score")
+    b12 = buttonStruct(x=190, y=740, width=120, height=30, name="Quit")
+
+    buts = [b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12]
+    x1 = int(input("input x of click: "))
+    y1 = int(input("input y of click: "))
+    for but in buts:
+        print(but.x)
+        if but.x <= x1 <= (but.x + but.width) and but.y <= y1 <= (but.y + but.height):
+            print("you clicked: ", but.name)
+            break
+
+def player_struct():
+    playerStruct = namedtuple("playerStruct","location inventory balance")
+    p1 = playerStruct(location= "Bristol", inventory= {"Cocaine":0, "Crack":0, "LSD":0, "Ecstasy":0,"Weed": 0}, balance=0)
+    arrofplayers = [p1]
+
+    for names in arrofplayers:
+        if names.location == "London":
+            print(names.inventory)
+
+def dictionary_places():
+    BirminghamDF = pd.read_csv("Birmingham.csv")
+    BristolDF = pd.read_csv("BristolDL.csv")
+    LondonDF = pd.read_csv("LondonDL.csv")
+    NottinghamDF = pd.read_csv("NottinghamDL.csv")
+
+    Dict = {1: ' BirminghamDF', 2: 'BristolDF ', 3: 'LondonDF', 4:'NottinghamDF'}
+
 
 
 def event_screen():
@@ -199,6 +244,8 @@ def Quit_button():
 
 Quit_button()
 
+def mouse_clicking():
+    pass
 
 def load_pricing(filename):
 
